a3ad8189a6a05bca98c637b6f5809d8f077ca095
DimaPekutko <dkefir03@gmail.com>
2022-05-23 21:29:04 +0300

    added logging

--- backend/librecritic/settings/base.py
+++ backend/librecritic/settings/base.py
@@ -141,6 +141,39 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {
+        "level": "INFO", 
+        "handlers": ["file"]
+    },
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "/var/log/django.log",
+            "formatter": "app",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True
+        },
+    },
+    "formatters": {
+        "app": {
+            "format": (
+                u"%(asctime)s [%(levelname)-1s] "
+                "(%(module)s.%(funcName)s) %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+}
+
 
 # AUTH_USER_MODEL="api.User"
 
--- backend/librecritic/urls.py
+++ backend/librecritic/urls.py
@@ -13,14 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
 from django.urls import path, re_path
 from django.views.generic import TemplateView
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    re_path(".*", TemplateView.as_view(template_name="index.html")),
 ]
+
+if os.environ.get("DJANGO_SETTINGS_MODULE") == "librecritic.settings.prod":
+    urlpatterns.append(
+        re_path(".*", TemplateView.as_view(template_name="index.html"))
+    )
